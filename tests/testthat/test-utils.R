160123a62ce838150b9b699301f22354423ded28
Gabor Csardi <csardi.gabor@gmail.com>
2016-04-25 22:41:26 +0100

    Refactor some utility test cases

--- tests/testthat/test-utils.R
+++ tests/testthat/test-utils.R
@@ -1,27 +1,96 @@
 
 context("utility functions")
 
+sw_cases <- function() {
+  list(
+    eq = list(
+      list(list(character(), character()), logical()),
+      list(list(character(), "x"), logical()),
+      list(list(character(), "foobar"), logical()),
+      list(list(c("", ""), ""), c(TRUE, TRUE)),
+      list(list(c("xxx", ""), c("x", "")), c(TRUE, TRUE)),
+      list(list(c("xxx", "foo"), c("x", "bar")), c(TRUE, FALSE)),
+      list(list(c("foobar", "bar"), c("foo", "bar")), c(TRUE, TRUE))
+    ),
+
+    true = list(
+      c("", ""),
+      c("xxx", ""),
+      c("xxx", "x"),
+      c("xxx", "xx"),
+      c("xxx", "xxx"),
+      c("foobar", "foo"),
+      c("foobar", "foobar")
+    ),
+
+    false = list(
+      c("", "x"),
+      c("x", "xx"),
+      c("foo", "b")
+    )
+  )
+}
+
 test_that("startswith", {
 
-  expect_equal(startswith(character(), character()), logical())
-  expect_equal(startswith(character(), "x"), logical())
-  expect_equal(startswith(character(), "foobar"), logical())
+  cases <- sw_cases()
 
-  expect_true(startswith("", ""))
-  expect_true(startswith("xxx", ""))
-  expect_true(startswith("xxx", "x"))
-  expect_true(startswith("xxx", "xx"))
-  expect_true(startswith("xxx", "xxx"))
-  expect_true(startswith("foobar", "foo"))
-  expect_true(startswith("foobar", "foobar"))
+  for (case in cases$eq) {
+    expect_equal(
+      startswith(case[[1]][[1]], case[[1]][[2]]),
+      case[[2]],
+      info = paste(unlist(case[[1]]), collapse = ",")
+    )
+  }
 
-  expect_false(startswith("", "x"))
-  expect_false(startswith("x", "xx"))
-  expect_false(startswith("foo", "b"))
+  for (case in cases$true) {
+    expect_true(
+      startswith(case[[1]], case[[2]]),
+      info = paste(case, collapse = ",")
+    )
+  }
 
-  expect_equal(startswith(c("", ""), ""), c(TRUE, TRUE))
-  expect_equal(startswith(c("xxx", ""), c("x", "")), c(TRUE, TRUE))
-  expect_equal(startswith(c("xxx", "foo"), c("x", "bar")), c(TRUE, FALSE))
-  expect_equal(startswith(c("foobar", "bar"), c("foo", "bar")), c(TRUE, TRUE))
+  for (case in cases$false) {
+    expect_false(
+      startswith(case[[1]], case[[2]]),
+      info = paste(case, collapse = ",")
+    )
+  }
+})
+
+rtol <- function(x) {
+  vapply(
+    lapply(strsplit(x, ""), rev),
+    paste,
+    FUN.VALUE = "",
+    collapse = ""
+  )
+}
+
+test_that("endswith", {
+
+  cases <- sw_cases()
+
+  for (case in cases$eq) {
+    expect_equal(
+      endswith(rtol(case[[1]][[1]]), rtol(case[[1]][[2]])),
+      case[[2]],
+      info = paste(unlist(case[[1]]), collapse = ",")
+    )
+  }
+
+  for (case in cases$true) {
+    expect_true(
+      endswith(rtol(case[[1]]), rtol(case[[2]])),
+      info = paste(case, collapse = ",")
+    )
+  }
+
+  for (case in cases$false) {
+    expect_false(
+      endswith(rtol(case[[1]]), rtol(case[[2]])),
+      info = paste(case, collapse = ",")
+    )
+  }
 
 })
